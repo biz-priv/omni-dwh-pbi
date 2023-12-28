7f05f57e269415a8dbe98eca91e2ea98ebb50171
Krithika Shivaram <krithika.shivaram@bizcloudexperts.com>
2023-12-28 02:06:00 -0600

    updated common functions

--- Jenkinsfile
+++ Jenkinsfile
@@ -42,6 +42,7 @@ pipeline {
                 anyOf {
                     branch 'master';
                     branch 'develop';
+                    branch 'feature/33988';
                 }
                 expression {
                     return true;
--- src/commonFunctions/helpers.js
+++ src/commonFunctions/helpers.js
@@ -2,20 +2,19 @@ const axios = require('axios');
 const AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
-function errorResponse(message) {
-  console.error("Error handler message: ", message);
-  return JSON.stringify({
+function errorResponse(httpStatus, message) {
+    console.error("Error handler message: ", message);
+    return JSON.stringify({
+      httpStatus: httpStatus,
       message: message
-  });
-}
+    });
+  }
 
-async function publishErrorMessageToSNS(error) {
-  const stage_val=process.env.ENV_STAGE_NAME
+async function publishErrorMessageToSNS(functionName,error) {
   const params = {
-    Message: `An error occurred in omni-coe-table-${stage_val}. Error details: ${error}.`,
+    Message: `An error occurred in ${functionName}. Error details: ${error}.`,
     TopicArn: process.env.ERROR_SNS_ARN,
   };
-
   try {
     await sns.publish(params).promise();
     console.log('Error message published to SNS successfully.');
